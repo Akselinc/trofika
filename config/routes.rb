eb8e993094a2dcf623f464946085ffcdbcafdf4b
Elias Glyptis <eliasglyptis@gmail.com>
2022-07-30 23:36:44 +0300

    completed authentication with email and password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'logout', edit: 'profile', registration: 'register' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
